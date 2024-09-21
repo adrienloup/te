78547a49aae7f2bb1c4159c32c537a51f1ccefcf
Adrien Loup <adloup@gmail.com>
2024-09-21 04:36:34 +0200

    feature(2): many changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,45 +8,45 @@ export default defineConfig({
     react(),
     VitePWA({
       manifest: {
-        short_name: 'te',
-        name: 'TE',
+        short_name: "te",
+        name: "TE",
         icons: [
           {
-            src: '/te/te-192x192.svg',
-            type: 'image/svg+xml',
-            sizes: '192x192',
+            src: "/te/te-192x192.svg",
+            type: "image/svg+xml",
+            sizes: "192x192",
           },
           {
-            src: '/te/te-256x256.svg',
-            type: 'image/svg+xml',
-            sizes: '256x256',
+            src: "/te/te-256x256.svg",
+            type: "image/svg+xml",
+            sizes: "256x256",
           },
           {
-            src: '/te/te-384x384.svg',
-            type: 'image/svg+xml',
-            sizes: '384x384',
+            src: "/te/te-384x384.svg",
+            type: "image/svg+xml",
+            sizes: "384x384",
           },
           {
-            src: '/te/te-512x512.svg',
-            type: 'image/svg+xml',
-            sizes: '512x512',
+            src: "/te/te-512x512.svg",
+            type: "image/svg+xml",
+            sizes: "512x512",
           },
         ],
-        start_url: '/te/',
-        background_color: '#242424',
-        display: 'standalone',
-        theme_color: '#00c48d',
-        description: 'A simple example of React with Atomic design.',
+        start_url: "/te/",
+        background_color: "#242424",
+        display: "standalone",
+        theme_color: "#00c48d",
+        description: "A simple example of React with Atomic design.",
       },
     }),
   ],
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['legacy-js-api'],
+        silenceDeprecations: ["legacy-js-api"],
         additionalData: '@use "./src/scss/base/breakpoints" as *;',
       },
     },
   },
-  base: '/te',
+  base: "/te",
 });
